ee2d3adb244fe9825c310aa6ffaf4603ff7ca660
Mery Khachikyan <merykhachikian@gmail.com>
2021-03-19 11:57:07 +0400

    new files assed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const random = require('random');
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs/promises');
 // 1
 // const promise = new Promise(function (resolve,reject){
 //     let num = random.int((min = 0),(max = 10));
@@ -26,3 +26,32 @@ const fs = require('fs');
 // });
 
 
+// fs.readFile('./config.json').then(function( data){
+//     http.createServer(function (req, res) {
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.write('Hello World!');
+//         res.end();
+//     }).listen(JSON.parse(data.toString()).port);
+// });
+
+// 3
+// fs.readFile('./input.txt').then(function (data){
+//     Promise.all([
+//
+//     ])
+// })
+async  function read(){
+    const data = await fs.readFile('./input.txt','utf-8');
+    const one = data.slice(0,data.length / 2);
+    const two = data.slice(data.length / 2);
+    await Promise.all([
+        fs.writeFile('./output1.txt',one),
+        fs.writeFile('./output2.txt',two)
+
+])
+}
+read().then(function (){
+    console.log('done');
+}).catch((err) =>{
+    console.log(err)
+})
