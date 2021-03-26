7052cb7865286121b7b8cf1157abad3e512b44f4
Mery Khachikyan <merykhachikian@gmail.com>
2021-03-26 15:56:22 +0400

    new files

--- homework-task2.js
+++ homework-task2.js
@@ -2,16 +2,12 @@ const http = require('http');
 const url = require('url');
 const  fs = require('fs');
 const stream = require('stream')
-// Create a server
-http.createServer( function (req, res) {
+http.createServer((req, res) => {
     const urlData = url.parse(req.url, true);
-    if(urlData.query.file &&  urlData.query.file === 'sunny.txt') {
-        fs.createReadStream(urlData.query.file,{encoding:'utf-8', highWaterMark:1}).on('data',(chunk => {
-            res.write(chunk);
-        }))
-    }
-    else {
+    if (urlData.query.file &&  urlData.query.file === 'sunny.txt'){
+        fs.createReadStream('./sunny.txt').pipe(res);
+    } else {
         res.statusCode = 404;
+        res.end('File NOt Found');
     }
-    res.end();
 }).listen(3000);
