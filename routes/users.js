0aab192bc7ec3b1ee53f253425d8dc64654b4209
Mery Khachikyan <merykhachikian@gmail.com>
2021-03-31 17:52:06 +0400

    new files

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,41 @@
 const  express = require('express');
 const router = express.Router();
+const upload = require('../middlewares/upload')
+// const multer = require('multer');
+// const  mimeType = require('mime-types');
+// // const  upload = multer({dest:'uploads/'});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads/')
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, file.fieldname + '-' + Date.now() + '.' + mimeType.extension(file.mimetype) )
+//     }
+// })
+//
+// const upload = multer({ storage: storage })
+// router.route('/').get((req,res)=>{
+//     res.end(req.method + ' ' + new Date().toDateString());
+// }).post((req,res)=>{
+//     res.end(req.method + ' ' + new Date().toDateString());
+// }).put((req,res)=>{
+//     res.end(req.method + ' ' + new Date().toDateString());
+// }).delete((req,res)=>{
+//     res.end(req.method + ' ' + new Date().toDateString());
+// });
 router.route('/').get((req,res)=>{
-    res.end('Method GET at ' + new Date().getHours() + ':'+ new Date().getMinutes()+ ':' + new Date().getMinutes());
-}).post((req,res)=>{
-    res.end('Method POST at '+ new Date().getHours() + ':'+ new Date().getMinutes()+ ':' + new Date().getMinutes());
-}).put((req,res)=>{
-    res.end('Method PUT at '+ new Date().getHours() + ':'+ new Date().getMinutes()+ ':' + new Date().getMinutes());
-}).delete((req,res)=>{
-    res.end('Method DELETE at '+ new Date().getHours() + ':'+ new Date().getMinutes()+ ':' + new Date().getMinutes());
+    res.end('All users list');
+}).post(upload.single('image'),(req,res)=>{
+    res.status(400).json({
+        success:true,
+        data: req.file
+    });
 });
-
+router.route('/:id').get((req,res)=>{
+    res.end('return user data special');
+}).put((req,res)=>{
+    res.end('edit');
+}).delete((req,res)=>{
+    res.end(req.method + ' ' + new Date().toDateString());
+});
 module.exports = router;
